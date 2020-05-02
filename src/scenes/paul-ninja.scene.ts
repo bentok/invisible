de851ed50b56a3ff88911bb16b94a44c55c25aa0
Paul Mojica <paulmojica@paul-mojica-macbook.home>
2020-05-02 00:58:04 -0400

    update webpack to not use https add menu for paul's ninja game add assets

---
+++ src/scenes/paul-ninja.scene.ts
@@ -0,0 +1,41 @@
+import { Scene } from "phaser";
+import { ISettingsConfig } from "../lib";
+import { getGameWidth, getGameHeight } from "../helpers";
+
+const sceneConfig: ISettingsConfig = {
+    active: false,
+    visible: false,
+    key: 'PaulNinja',
+  };
+export class PaulNinjaScene extends Scene {
+    private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  private image: Phaser.Physics.Matter.Sprite;
+
+  constructor() {
+    super(sceneConfig);
+  }
+
+  create() {
+    this.image = this.matter.add.sprite(0, 0, 'foobar'); // Image name specified in boot.scene.ts
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.matter.world.setBounds(0, 0, getGameWidth(this), getGameHeight(this), 0, true, false, true);
+  }
+
+  update() {
+    if (this?.cursorKeys?.left?.isDown) {
+      this.image.setVelocityX(-10);
+    } else if (this?.cursorKeys?.right?.isDown) {
+      this.image.setVelocityX(10);
+    } else {
+      this.image.setVelocityX(0);
+    }
+    if (this?.cursorKeys?.up?.isDown) {
+      this.image.setVelocityY(-10);
+    } else if (this?.cursorKeys?.down?.isDown) {
+      this.image.setVelocityY(10);
+    } else {
+      this.image.setVelocityY(0);
+    }
+  }
+}
